bfeeb9653650dc69bd467d3bbbbfbbf177ee5948
Dominik Kinal <kinaldominik@gmail.com>
2015-08-11 21:56:43 +0200

    klikniecie na kategorie w menu przenosi na najwazniejsza strone kategorii

--- application/config/menu.php
+++ application/config/menu.php
@@ -4,6 +4,7 @@ return array(
 	'login' => array(
 		'<i class="glyphicon glyphicon-home"></i>' => array(
 			'name' => 'Biuro',
+			'url' => 'podglad',
 			'<i class="fa fa-eye"></i>' => array('name' => 'Podgląd', 'url' => 'podglad'),
 			'<i class="fa fa-th-list"></i>' => array('name' => 'Zlecenia', 'url' => 'zlecenia2', 'badge' => 'menu_zlecen'),
 			'<i class="glyphicon glyphicon-briefcase"></i>' => array('name' => 'Kadry', 'url' => 'kadry'),
@@ -17,6 +18,7 @@ return array(
 
 		'<i class="glyphicon glyphicon-wrench"></i>' => array(
 			'name' => 'Hangar',
+            'url' => 'samoloty',
 			'<i class="glyphicon glyphicon-plane"></i>' => array('name' => 'Samoloty', 'url' => 'samoloty'),
 			'<i class="fa fa-cogs"></i>' => array('name' => 'Warsztat', 'url' => 'warsztat'),
 			'<i class="fa fa-shopping-cart"></i>' => array('name' => 'Sklep', 'url' => 'sklep'),
@@ -26,6 +28,7 @@ return array(
 
 		'<i class="glyphicon glyphicon-user"></i>' => array(
 			'name' => 'Konto',
+            'url' => 'profil',
 			'<i class="fa fa-users"></i>' => array('name' => 'Kontakty', 'url' => 'kontakty', 'badge' => 'nowych_kontaktow'),
 			'<i class="fa fa-envelope"></i>' => array('name' => 'Poczta', 'url' => 'poczta', 'badge' => 'nowych_wiadomosci'),
 			'<i class="fa fa-share-alt"></i>' => array('name' => 'Poleceni', 'url' => 'referrals'),
